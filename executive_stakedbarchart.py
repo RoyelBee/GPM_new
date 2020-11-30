57ad42f5d2c28d805ccbb7f45279be0bea8e6f20
Royel <royelsoft@gmail.com>
2020-11-30 12:57:44 +0600

    Add percent values and brand name in executive_stakedbarchart.py

--- executive_stakedbarchart.py
+++ executive_stakedbarchart.py
@@ -2,28 +2,67 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pandas as pd
+import numpy as np
+import xlrd
+
 data = pd.read_excel('testdata.xlsx')
 all_data = []
 for i in range(data.shape[0]):
     all_data.append((data.loc[i].tolist())[1:])
 
-print(all_data)
+
+
+writer = pd.ExcelWriter('new_testdata2.xlsx', engine='xlsxwriter')
+for j in range(0, np.shape(all_data)[1]):
+    vars()['new_list' + str(j)] = []
+    vars()['new_list' + str(j) + 'per'] = []
+    vars()['new_list' + str(j) + 'sum'] = 0
+    for i in range(0, np.shape(all_data)[0]):
+        vars()['new_list' + str(j)].append(all_data[i][j])
+    vars()['new_list' + str(j) + 'sum'] = sum(vars()['new_list' + str(j)])
+    vars()['new_list' + str(j) + 'per'] = np.divide(vars()['new_list' + str(j)],
+                                                    vars()['new_list' + str(j) + 'sum']) * 100
+    vars()['new_list' + str(j) + 'df'] = pd.DataFrame(vars()['new_list' + str(j) + 'per'])
+
+    vars()['new_list' + str(j) + 'df'].to_excel(writer, sheet_name='Sheet1', index=False, startcol=j, startrow=0)
+writer.save()
+
+new_data = pd.read_excel('new_testdata2.xlsx')
+new_all_data = []
+
+for i in range(new_data.shape[0]):
+    new_all_data.append((new_data.loc[i].tolist()[0:]))
+
 # # # --------------------- Creating fig-----------------------------------------
-# # # From raw value to percentage
+# # From raw value to percentage
 
 
-# plot
 barWidth = 0.75
 executive_names = data.columns.tolist()[1:]
+
+
 # print(executive_names)
+def zero_checker(val):
+    if val == 0.0:
+        val = ''
+    else:
+        val = str(val)+'%'
+    return val
+
+def zero_label_checker(val, label):
+    if val == 0.0:
+        lable = ''
+    else:
+        lable = label
+    return lable
 
 plt.subplots(figsize=(12.8, 6))
 
+
 def plot_stacked_bar(data, series_labels, category_labels=None,
-                     show_values=False, value_format="{}", y_label=None,
+                     show_values=True, value_format="{}", y_label=None,
                      colors=None, grid=False, reverse=False):
-
-
     ny = len(data[0])
     ind = list(range(ny))
 
@@ -32,21 +71,21 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
 
     data = np.array(data)
 
-    if reverse:
-        data = np.flip(data, axis=1)
-        category_labels = reversed(category_labels)
+    # if reverse:
+    #     data = np.flip(data, axis=1)
+    #     category_labels = reversed(category_labels)
 
     for i, row_data in enumerate(data):
         color = colors[i] if colors is not None else None
         axes.append(plt.bar(ind, row_data, bottom=cum_size,
-                                label=series_labels[i], color=color))
+                            label=series_labels[i], color=color))
         cum_size += row_data
 
     if category_labels:
         plt.xticks(ind, category_labels, rotation=0, fontsize=14)
 
-    if y_label:
-        plt.ylabel(y_label)
+    # if y_label:
+    #     plt.ylabel(y_label)
 
     # plt.legend()
 
@@ -54,30 +93,46 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
     #     plt.grid()
 
     if show_values:
+        i = 0
         for axis in axes:
+
             for bar in axis:
+
                 w, h = bar.get_width(), bar.get_height()
-                plt.text(bar.get_x() + w / 2, bar.get_y() + h / 2,
-                    value_format.format(h), ha="center",
-                    va="center", rotation=0, fontsize=14, fontweight='bold')
+                plt.text(bar.get_x() + w / 2, bar.get_y() + h /3,
+                         zero_checker(round(h, 2)), ha="center",
+                         va="center", rotation=0, fontsize=14, fontweight='bold')
+
+                plt.text(bar.get_x() + w / 2, bar.get_y() + h/1.3,
+                         zero_label_checker(h, series_labels[i]), ha="center",
+                         va="center", rotation=0, fontsize=14, fontweight='bold')
+            i= i+1
+            print(i)
 
 
 series_labels = data['Brands'].tolist()
+print(series_labels)
+
+
+colors = ['#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
+          '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd']
 
 plot_stacked_bar(
-    all_data,
+    new_all_data,
     series_labels,
     category_labels=executive_names,
     show_values=True,
-    value_format='{:.0f}',
-    colors=['#31c377', '#f4b300', 'red', '#96ff00', '#0089ff', '#e500ff', '#00ffd8','red', '#96ff00', '#0089ff', '#e500ff', '#00ffd8']
+    value_format='{:.2f}' + '%',
+    colors=colors
+
+
 )
 
-# plt.xlabel("Branch Name", fontweight='bold', fontsize=12)
-plt.ylabel("Target", fontweight='bold', fontsize=14)
+plt.xlabel("Executive Name", fontweight='bold', fontsize=14)
+plt.ylabel("Percent %", fontweight='bold', fontsize=14)
 plt.title('Executives Brand wise Sales', fontsize=16, fontweight='bold', color='#3e0a75')
 # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.085),
 #                fancybox=True, shadow=True, ncol=7)
 
-# plt.show()
-plt.savefig('testfig.png')
+plt.show()
+# plt.savefig('testfig.png')
