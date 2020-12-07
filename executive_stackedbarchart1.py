826009f1f0245a592a63d1526fb220c3d249fd8b
Royel <royelsoft@gmail.com>
2020-12-07 18:50:20 +0600

    A

--- executive_stackedbarchart1.py
+++ executive_stackedbarchart1.py
@@ -1,118 +1,106 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-
-import pandas as pd
 import numpy as np
-import xlrd
-
 import pandas as pd
-import path as dir
 import Functions.db_connection as dbc
 
 sql = """
-DECLARE @cols NVARCHAR (MAX)
+    DECLARE @cols NVARCHAR (MAX)
 
-SELECT @cols = COALESCE (@cols + ',[' + [executive shortname] + ']', '[' + [executive shortname] + ']')
-               FROM
-               (
-                    SELECT distinct [executive shortname] from [dbo].[GPMExecutive_ShortName] where gpmname= 'Mr. Fazal Mohammad Tanim'
-               ) PV
-               ORDER BY [executive shortname]
-DECLARE @query NVARCHAR(MAX)
-SET @query ='Select * from
-        (select *
-        from
-        (
-      select sum(extinvmisc) as sale,Item.brand,item.itemno,[Executive ShortName] as Exe from
-(select * from oesalesdetails where  
-transtype =1 and transdate>=convert(varchar(8),getdate(), 112)) as Sale
-left join
-(select * from prinfoskf ) as Item
-on sale.item=item.itemno
-
-left join
-(select * from [GPMExecutive_ShortName]) as Exe on
-exe.ExecutiveName=item.cp01
-group by [Executive ShortName],item.exid,item.brand,item.ITEMno
-        ) src
-        pivot
-        (Max(Sale)
-       for EXE in (' + @cols + ')) AS piv
-        ) as TblSale
-              left join
+    SELECT @cols = COALESCE (@cols + ',[' + [executive shortname] + ']', '[' + [executive shortname] + ']')
+                   FROM
+                   (
+                        SELECT distinct [executive shortname] from [dbo].[GPMExecutive_ShortName] where gpmname= 'Mr. Fazal Mohammad Tanim'
+                   ) PV
+                   ORDER BY [executive shortname]
+    DECLARE @query NVARCHAR(MAX)
+    SET @query ='Select * from
             (select *
-        from
-(select [Executive ShortName] as Exe,item.brand,item.itemno as itemno,sum(trgval) as Target from
-(select * from [ARCSECONDARY].[dbo].[PRODUCT_WISE_TRG] where yrm=convert(varchar(6),getdate(), 112)) as Tar
-right join
-(select * from prinfoskf  ) as Item
-on tar.exid=item.exid
-and item.itemno=tar.item
-left join
-(select * from [GPMExecutive_ShortName]) as Exe on
-exe.ExecutiveName=item.cp01
-group by tar.exid,item.itemno,[Executive ShortName] ,item.Brand
-        ) src
-        pivot
-        (
-        sum(Target)
-        for EXE in (' + @cols + '
-         )
-        ) AS piv) as tblsTarget
-        on (tblsTarget.itemno=TblSale.itemno)'
-
-EXEC SP_EXECUTESQL @query
-"""
+            from
+            (
+          select sum(extinvmisc) as sale,Item.brand,[Executive ShortName] as Exe from
+    (select * from oesalesdetails where  
+    transtype =1 and transdate>=convert(varchar(8),getdate(), 112)) as Sale
+    left join
+    (select * from prinfoskf ) as Item
+    on sale.item=item.itemno
+    
+    left join
+    (select * from [GPMExecutive_ShortName]) as Exe on
+    exe.ExecutiveName=item.cp01
+    group by [Executive ShortName],item.exid,item.brand
+            ) src
+            pivot
+            (Max(Sale)
+           for EXE in (' + @cols + ')) AS piv
+            ) as TblSale
+                  left join
+                (select *
+            from
+    (select [Executive ShortName] as Exe,item.brand,sum(trgval) as Target from
+    (select * from [ARCSECONDARY].[dbo].[PRODUCT_WISE_TRG] where yrm=convert(varchar(6),getdate(), 112)) as Tar
+    right join
+    (select * from prinfoskf  ) as Item
+    on tar.exid=item.exid
+    and item.itemno=tar.item
+    left join
+    (select * from [GPMExecutive_ShortName]) as Exe on
+    exe.ExecutiveName=item.cp01
+    group by tar.exid,[Executive ShortName] ,item.Brand
+            ) src
+            pivot
+            (
+            sum(Target)
+            for EXE in (' + @cols + '
+             )
+            ) AS piv) as tblsTarget
+            on (tblsTarget.brand=TblSale.brand)'
+    
+    EXEC SP_EXECUTESQL @query
+    """
 
 df = pd.read_sql(sql, dbc.connection)
 
 df.to_csv('initialdata.csv', index=False)
-coluns = df.columns.tolist()[1:]
 
 df = pd.read_csv('initialdata.csv')
-# print('Total col = ', len(df.columns))
 
 df = df.reindex(sorted(df.columns), axis=1)
 
-
-
 df1 = df[['brand'] + [col for col in df.columns if col != 'brand']]
 
-
 # print(df1.columns)
-df1 = df1.iloc[:, :-3]
+df1 = df1.iloc[:, :-1]
 master_col = []
 coluns = df1.columns.tolist()[1:]
 
 # # Arrange columns name with .S for sales and .T for Target
 for i in range(len(coluns)):
-    if i%2 == 0:
-        col = coluns[i]+' .S'
+    if i % 2 == 0:
+        col = coluns[i] + ' .S'
         master_col.append(col)
     else:
-        col = coluns[i].replace(".1", "")+' .T'
+        col = coluns[i].replace(".1", "") + ' .T'
         master_col.append(col)
 
 # print(master_col)
 
 
 threshLinit = (len(df1.columns)) - 3
-print(threshLinit)
+# print(threshLinit)
 df1 = df1[df1.isnull().sum(axis=1) <= threshLinit]
 df1.to_csv('master_executive_target_sales_data.csv', index=False)
 
-# df = pd.read_csv('master_executive_target_sales_data.csv')
+# # Fill all blank cell with 0
 df = df1.fillna(0)
-l = df.columns.tolist()
-Colvalues = l[1:]
 
+# # Short data by values (All Sales and Target)
+colv = df.columns.tolist()
+Colvalues = colv[1:]
 df.sort_values(by=Colvalues, inplace=True)
 
 df.to_csv('master_executive_target_sales_data.csv', index=False)
-# print('Executive Stacked Bar-chart Initial Data Saved ')
 
-
+# # Now append all data in a list and process it for stacked barchart
 data = pd.read_csv('master_executive_target_sales_data.csv')
 all_data = []
 for i in range(data.shape[0]):
@@ -140,7 +128,6 @@ for i in range(new_data.shape[0]):
     new_all_data.append((new_data.loc[i].tolist()[0:]))
 
 # # # --------------------- Creating fig-----------------------------------------
-# # From raw value to percentage
 
 barWidth = 0.90
 executive_names = data.columns.tolist()[1:]
@@ -151,7 +138,7 @@ def zero_checker(val):
     if val == 0.0:
         val = ''
     else:
-        val = str(val) #+ '%'
+        val = str(val)  # + '%'
     return val
 
 
@@ -159,15 +146,15 @@ def zero_label_checker(val, label):
     if val == 0.0:
         lable = ''
     else:
-        lable = label # + '\n' + str(round(val, 2)) + '%'
+        lable = label + '\n' + str(round(val, 2)) + '%'
         # lable =  str(round(val, 2)) #+ '%'
     return lable
 
 
-plt.subplots(figsize=(20, 8))
+plt.subplots(figsize=(18, 10))
 
 
-def plot_stacked_bar(data, series_labels, category_labels=None,
+def plot_stacked_bar(data, brands, category_labels=None,
                      show_values=True, value_format="{}", y_label=None,
                      colors=None, grid=True, reverse=False):
     ny = len(data[0])
@@ -178,6 +165,7 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
 
     data = np.array(data)
 
+
     # if reverse:
     #     data = np.flip(data, axis=1)
     #     category_labels = reversed(category_labels)
@@ -185,13 +173,13 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
     for i, row_data in enumerate(data):
         # color = colors[i] if colors is not None else None
         axes.append(plt.bar(ind, row_data, bottom=cum_size,
-                            label=series_labels[i]
+                            label=brands[i]
                             # , color=color
                             ))
         cum_size += row_data
 
     if category_labels:
-        plt.xticks(ind, master_col, rotation=90, fontsize=14, fontweight='bold')
+        plt.xticks(ind, master_col, rotation=45, fontsize=14, fontweight='bold')
 
     # if y_label:
     #     plt.ylabel(y_label)
@@ -208,28 +196,29 @@ def plot_stacked_bar(data, series_labels, category_labels=None,
             for bar in axis:
                 w, h = bar.get_width(), bar.get_height()
                 # plt.text(bar.get_x() + w / 2, bar.get_y() + h / 2,
-                #          zero_label_checker(h, series_labels[i]), ha="center",
+                #          zero_label_checker(h, brands[i]), ha="center",
                 #          va="center", rotation=0, fontsize=14)
 
-                plt.text(bar.get_x() + w / 2, bar.get_y() + h/1.3,
-                         zero_label_checker(h, series_labels[i]), ha="center",
+                plt.text(bar.get_x() + w / 2, bar.get_y() + h / 1.5,
+                         zero_label_checker(h, brands[i]), ha="center",
                          va="center", rotation=0, fontsize=12)
-            i= i+1
-            # print(i)
+            i = i + 1
 
 
-series_labels = data['brand'].tolist()
-# print(series_labels)
+brands = data['brand'].tolist()
 
-colors = ['#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
-          '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd', '#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
-          '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd','#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
-          '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd','#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
-          '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd']
+# colors = ['#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97', '#c6ff76', '#6abafe', '#e500ff',
+#           '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd', '#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97',
+#           '#c6ff76', '#6abafe', '#e500ff',
+#           '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd', '#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97',
+#           '#c6ff76', '#6abafe', '#e500ff',
+#           '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd', '#ff8f00', '#96ff00', '#e4ff00', '#e7bcec', '#b3fff3', '#f1ca97',
+#           '#c6ff76', '#6abafe', '#e500ff',
+#           '#6afeae', '#d8ef3c', '#3cd8ef', '#ffd1dd']
 
 plot_stacked_bar(
     new_all_data,
-    series_labels,
+    brands,
     category_labels=executive_names,
     show_values=True,
     value_format='{:.1f}' + '%'
@@ -245,3 +234,4 @@ plt.title('Executives Brand wise Sales', fontsize=16, fontweight='bold', color='
 
 # plt.show()
 plt.savefig('mainexecutive.png')
+print('Brand wise Executives Target & Sales Figure Generated')
